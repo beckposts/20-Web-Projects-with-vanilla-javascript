bdea37ef4421b72748b61b402be5166c57e1acd5
doordion <doordion@gmail.com>
2022-07-08 14:06:13 +0530

    Save slected movie & ticket price

--- movie-seat-booking/script.js
+++ movie-seat-booking/script.js
@@ -8,6 +8,11 @@ let ticketPrice = +movieselect.value; // convert to num
 
 // console.log(typeof ticketPrice);
 
+// Save slected movie & ticket price
+function setMovieData(movieIndex, moviePrice) {
+  localStorage.setItem('selectedMovieIndex', movieIndex);
+  localStorage.setItem('selectedMoviePrice', moviePrice);
+}
 // Update total & count
 function updateSelectedCount() {
   const selectedSeats = document.querySelectorAll('.row .seat.selected');
@@ -40,6 +45,8 @@ function updateSelectedCount() {
 // Movie Select Event
 movieselect.addEventListener('change', (e) => {
   ticketPrice = +e.target.value;
+  setMovieData(e.target.selectedIndex, e.target.value);
+  // console.log(e.target.selectedIndex, e.target.value);
   updateSelectedCount();
 });
 
